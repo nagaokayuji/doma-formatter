450f3845d5ae048c5356c94b25228dd04cee8031
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-04-24 18:28:06 +0900

    feat: range formatting

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { format } from "./formatter/format";
+import { format, TConfig } from "./formatter/format";
 
+/** target file */
 const sqlSelector: vscode.DocumentSelector = [
   { scheme: "file", language: "sql" },
 ];
-const getSettings = () => {
+
+/** configure format options */
+const getSettings = (options: vscode.FormattingOptions): TConfig => {
   const settings: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(
     "doma-sql-formatter"
   );
-  return { toUppercase: settings.get("toUppercase") != false };
+  return {
+    uppercase: settings.get("toUppercase") !== false,
+    indent: options.insertSpaces ? " ".repeat(options.tabSize) : "\t",
+  };
 };
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  vscode.languages.registerDocumentFormattingEditProvider(sqlSelector, {
-    provideDocumentFormattingEdits(
-      document: vscode.TextDocument
-    ): vscode.ProviderResult<vscode.TextEdit[]> {
-      const text: string = document.getText();
-      const lineEnd: number = document.lineCount - 1;
-      const start: vscode.Position = new vscode.Position(0, 0);
-      const end: vscode.Position = new vscode.Position(
-        lineEnd,
-        document.lineAt(lineEnd).text.length
-      );
-      const range: vscode.Range = new vscode.Range(start, end);
-      return [
-        vscode.TextEdit.replace(range, format(text, getSettings().toUppercase)),
-      ];
-    },
+  vscode.languages.registerDocumentRangeFormattingEditProvider(sqlSelector, {
+    provideDocumentRangeFormattingEdits: (document, range, options) => [
+      vscode.TextEdit.replace(
+        range,
+        format(document.getText(range), getSettings(options))
+      ),
+    ],
   });
 }
 
