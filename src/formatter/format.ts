6e228564d8c3915356e2468b644b12ed5f54d8f8
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-04-24 18:27:53 +0900

    add: indent options

--- src/formatter/format.ts
+++ src/formatter/format.ts
@@ -1,10 +1,17 @@
 import * as vscode from "vscode";
 import * as sqlFormatter from "sql-formatter";
 
+/** configuration oprions */
+export type TConfig = {
+  uppercase: boolean;
+  indent: string;
+};
+
 /** formatter */
-export const format = (text: string, isUppercase: boolean): string => {
+export const format = (text: string, options: TConfig): string => {
   let formattedText = sqlFormatter.format(text, {
-    uppercase: isUppercase,
+    uppercase: options.uppercase,
+    indent: options.indent,
     isDoma: true,
   });
   return formattedText;
